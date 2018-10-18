3580122acbdf61dd7c914a63da842d8528b2a505
zhuangxincheng <18310763990@163.com>
2018-10-18 15:01:01 +0800

    install

---
+++ src/main/java/com/confing/server/ConfingServerApplication.java
@@ -0,0 +1,12 @@
+package com.confing.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfingServerApplication.class, args);
+	}
+}
